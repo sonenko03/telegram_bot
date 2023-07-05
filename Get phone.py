feda596341d3b17da4c390fe3c528fd24d8ddd4f
sonenko03 <serwret@yandex.ru>
2023-07-06 00:32:07 +0700

    Add files via upload

---
+++ Get phone.py
@@ -0,0 +1,31 @@
+@app.route('/GetPhone', methods=['POST'])
+def getphone():
+        ret = {}
+        ret["UID"] = []
+        ret["Phone"] = []
+        json_data = request.get_json()
+        uids = json_data["UIDS"]
+        if type(uids) == type(str(" ")):
+                cheking_phone = ("""SELECT telephone FROM destination WHERE id = %s""")
+                cursor.execute(cheking_phone, [uids])
+                phone = cursor.fetchall()
+                ret["UID"].append(uids)
+                if phone == "null":
+                        ret["Phone"].append("Phone not found")
+                elif phone:
+                        ret["Phone"].append(phone)
+                else:
+                        ret["Phone"].append("Phone not found")
+        else:
+                for uid in json_data["UIDS"]:
+                        cheking_phone = ("""SELECT telephone FROM destination WHERE id = %s""")
+                        cursor.execute(cheking_phone, [uid])
+                        phone = cursor.fetchall()
+                        ret["UID"].append(uid)
+                        if phone == "null":
+                                ret["Phone"].append("Phone not found")
+                        elif phone:
+                                ret["Phone"].append(phone[0])
+                        else:
+                                ret["Phone"].append("Phone not found")
+        return jsonify(ret)
